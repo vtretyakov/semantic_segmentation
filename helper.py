411405d325da22675c5d8ff629fd6a9e2974c4e7
vtretyakov <vtretyakov@synapticon.com>
2018-08-10 19:47:14 +0200

    fix missing argument

--- helper.py
+++ helper.py
@@ -146,7 +146,7 @@ def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_p
     for name, image in image_outputs:
         scipy.misc.imsave(os.path.join(output_dir, name), image)
 
-def gen_video_output(sess, image_shape, logits, keep_prob, input_image):
+def gen_video_output(sess, image_shape, logits, keep_prob, image_pl, input_image):
     image = scipy.misc.imresize(input_image, image_shape)
     im_softmax = sess.run(
         [tf.nn.softmax(logits)],
